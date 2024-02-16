125bcde5bd4e25a42f50ec3c34037499a5db6bd8
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-02-16 15:20:57 -0500

    [ADD] deploy to gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
 
+const PORT='4321'
+const astroConfig = {
+  site: '',
+  base: '',
+}
+const running_Script = process.env.npm_lifecycle_script || ''
+const isProduction = running_Script?.includes('astro build')
+
+// node astro.config.mjs
+// console.log(isProduction ? 'tiene valor' : 'es falso o undefined')
+
+if (isProduction) {
+  astroConfig.site = 'https://deglan-rivas.github.io'
+  astroConfig.base = '/frontend-astro-portafolio'
+} else {
+  astroConfig.site = `http://localhost:${PORT}`
+  astroConfig.base = '/'
+}
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig(astroConfig);
